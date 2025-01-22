2d357a66849ca6c1f098926d9576ffb358bb20b6
dea1j <jdayo2012@gmail.com>
2025-01-22 11:29:03 +0100

    product

---
+++ eCommerce.SharedLibrarySolution/eCommerce.SharedLibrarySolution/DependencyInjection/SharedServiceContainer.cs
@@ -0,0 +1,50 @@
+﻿
+using eCommerce.SharedLibrary.Middleware;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+
+namespace eCommerce.SharedLibrary.DependencyInjection
+{
+    public static class SharedServiceContainer
+    {
+        public static IServiceCollection AddSharedServices<TContext>(this IServiceCollection services, 
+            IConfiguration config, string fileName) where TContext: DbContext
+        {
+            // Add generic db context
+
+            services.AddDbContext<TContext>(option => option.UseSqlServer(
+                config
+                .GetConnectionString("eCommerceConnection"), sqlserverOption =>
+                sqlserverOption.EnableRetryOnFailure()));
+
+            // Configure Serilog Logging
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Information()
+                .WriteTo.Debug()
+                .WriteTo.Console()
+                .WriteTo.File(path: $"{fileName}-text",
+                restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Information,
+                outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {message:lj}{NewLine}{Exception}",
+                rollingInterval: RollingInterval.Day)
+                .CreateLogger();
+
+            // Add JWT authentication Scheme
+            JWTAuthenticationScheme.AddJWTAuthenticationScheme(services, config);
+            return services;
+        }
+
+        public static IApplicationBuilder UseSharedPolicies(IApplicationBuilder app)
+        {
+            // Use global exception
+            app.UseMiddleware<GlobalException>();
+
+            // Register middleware to block all ousiders
+            app.UseMiddleware<ListenToOnlyApiGateway>();
+
+            return app;
+        }
+    }
+}
---
+++ eCommerce.SharedLibrarySolution/eCommerce.SharedLibrarySolution/Middleware/GlobalException.cs
@@ -0,0 +1,81 @@
+﻿
+using eCommerce.SharedLibrary.Logs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Text.Json;
+
+namespace eCommerce.SharedLibrary.Middleware
+{
+    public class GlobalException(RequestDelegate next)
+    {
+        public async Task InvokeAsync(HttpContext context)
+        {
+            // Declare default variables
+            string message = "Sory, internal server error occured. Kindly try again";
+            int statusCode = (int)HttpStatusCode.InternalServerError;
+            string title = "Error";
+
+            try
+            {
+                await next(context);
+
+                // Check if Response is too many request // 429 status code
+                if (context.Response.StatusCode == StatusCodes.Status429TooManyRequests)
+                {
+                    title = "Warning";
+                    message = "Too many request made";
+                    statusCode = (int)StatusCodes.Status429TooManyRequests;
+                    await ModifyHeader(context, title, message, statusCode);
+                };
+
+                // Check if response is unauthorized // 401 status code
+                if (context.Response.StatusCode == StatusCodes.Status401Unauthorized)
+                {
+                    title = "Alert";
+                    message = "You are not authorized to access";
+                    await ModifyHeader(context, title, message, statusCode);
+                }
+
+                // Check if response is forbidden // 403 status code
+                if (context.Response.StatusCode == StatusCodes.Status403Forbidden)
+                {
+                    title = "Out of Access";
+                    message = "You are not allowed to access";
+                    statusCode = (int)StatusCodes.Status403Forbidden;
+                    await ModifyHeader(context, title, message, statusCode);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                // Log Original Exceptions /File, Debugger, Console
+                LogException.LogExceptions(ex);
+
+                // Check if exception is Timeout
+                if (ex is TaskCanceledException || ex is TimeoutException)
+                {
+                    title = "Out of time";
+                    message = "Request timeout.. Try gain";
+                    statusCode = StatusCodes.Status408RequestTimeout;
+                }
+
+                // If Exception is caught || If none of the exception do the default
+                await ModifyHeader(context, title, message, statusCode);
+            }
+        }
+
+        private static async Task ModifyHeader(HttpContext context, string title, string message, int statusCode)
+        {
+            // display scary-free messag to client
+            context.Response.ContentType = "application/json";
+            await context.Response.WriteAsync(JsonSerializer.Serialize(new ProblemDetails()
+            {
+                Detail = message,
+                Status = statusCode,
+                Title = title
+            }), CancellationToken.None);
+            return;
+        }
+    }
+}
