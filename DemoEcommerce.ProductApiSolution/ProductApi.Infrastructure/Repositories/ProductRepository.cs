ece30642b8f819348474f666904eafe2c677682b
dea1j <jdayo2012@gmail.com>
2025-01-22 20:21:53 +0100

    product push

---
+++ DemoEcommerce.ProductApiSolution/ProductApi.Infrastructure/Repositories/ProductRepository.cs
@@ -0,0 +1,54 @@
+﻿using eCommerce.SharedLibrary.Logs;
+using eCommerce.SharedLibrary.Responses;
+using ProductApi.Application.Interfaces;
+using ProductApi.Domain.Entities;
+using ProductApi.Infrastructure.Data;
+using System.Linq.Expressions;
+
+namespace ProductApi.Infrastructure.Repositories
+{
+    public class ProductRepository(ProductDbContext context) : IProduct
+    {
+        public async Task<Response> CreateAsync(Product entity)
+        {
+            throw new NotImplementedException();
+            //try
+            //{
+            //    var getProduct = await GetByAsync(_ => _.Name!.Equals(entity.Name));
+            //}
+            //catch (Exception ex)
+            //{
+            //    // Log original exception
+            //    LogException.LogExceptions(ex);
+
+            //    // Display scary-free message to client
+            //    return new Response(false, "Error occured while adding new product");
+            //}
+        }
+
+        public Task<Response> DeleteAsync(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product> FindByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Product>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product> GetByAsync(Expression<Func<Product, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response> UpdateAsync(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
