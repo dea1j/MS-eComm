5dc1acebd3134fe2af6d9d9a06b7be307de89e1f
dea1j <jdayo2012@gmail.com>
2025-01-23 18:59:08 +0100

    part product repo

--- DemoEcommerce.ProductApiSolution/ProductApi.Infrastructure/Repositories/ProductRepository.cs
+++ DemoEcommerce.ProductApiSolution/ProductApi.Infrastructure/Repositories/ProductRepository.cs
@@ -1,5 +1,6 @@
 ﻿using eCommerce.SharedLibrary.Logs;
 using eCommerce.SharedLibrary.Responses;
+using Microsoft.EntityFrameworkCore;
 using ProductApi.Application.Interfaces;
 using ProductApi.Domain.Entities;
 using ProductApi.Infrastructure.Data;
@@ -11,10 +12,74 @@ namespace ProductApi.Infrastructure.Repositories
     {
         public async Task<Response> CreateAsync(Product entity)
         {
+            try
+            {
+                // check if product already exist
+                var getProduct = await GetByAsync(_ => _.Name!.Equals(entity.Name));
+                if (getProduct is not null && !string.IsNullOrEmpty(getProduct.Name))
+                    return new Response(false, $"{entity.Name} already added");
+
+                var currentEntity = context.Products.Add(entity).Entity;
+                await context.SaveChangesAsync();
+                if (currentEntity is not null && currentEntity.Id > 0)
+                    return new Response(true, $"{entity.Name} added to database successfully");
+                else
+                    return new Response(false, $"Error occured while adding {entity.Name}");
+            }
+            catch (Exception ex)
+            {
+                // Log original exception
+                LogException.LogExceptions(ex);
+
+                // Display scary-free message to client
+                return new Response(false, "Error occured while adding new product");
+            }
+        }
+
+        public async Task<Response> DeleteAsync(Product entity)
+        {
+            try
+            {
+                var product = await FindByIdAsync(entity.Id);
+                if(product is null)
+                    return new Response(false, $"{entity.Name} not found");
+                
+                context.Products.Remove(entity);
+                await context.SaveChangesAsync();
+                return new Response(true, $"{entity.Name} deleted successfully");
+            }
+            catch (Exception ex)
+            {
+                // Log original exception
+                LogException.LogExceptions(ex);
+
+                // Display scary-free message to client
+                return new Response(false, "Error occured while adding new product");
+            }
+        }
+
+        public async Task<Product> FindByIdAsync(int id)
+        {
+            try
+            {
+                var product = await context.Products.FindAsync(id);
+                return product is not null ? product : null;
+            }
+            catch (Exception ex)
+            {
+                // Log original exception
+                LogException.LogExceptions(ex);
+
+                throw new Exception("Error occured while retrieving product");
+            }
+        }
+
+        public async Task<IEnumerable<Product>> GetAllAsync()
+        {
             throw new NotImplementedException();
             //try
             //{
-            //    var getProduct = await GetByAsync(_ => _.Name!.Equals(entity.Name));
+            //    var product = await context.Products.ToListAsync()
             //}
             //catch (Exception ex)
             //{
@@ -22,23 +87,8 @@ namespace ProductApi.Infrastructure.Repositories
             //    LogException.LogExceptions(ex);
 
             //    // Display scary-free message to client
-            //    return new Response(false, "Error occured while adding new product");
+            //    throw new InvalidOperationException("Error Occured while retrieving products");
             //}
-        }
-
-        public Task<Response> DeleteAsync(Product entity)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<Product> FindByIdAsync(int id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<IEnumerable<Product>> GetAllAsync()
-        {
-            throw new NotImplementedException();
         }
 
         public Task<Product> GetByAsync(Expression<Func<Product, bool>> predicate)
